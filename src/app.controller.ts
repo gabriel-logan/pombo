a4c1884763186a4fe94cebf87982ebf8df4aadf5
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-09-08 16:15:57 -0300

    feat: add Handlebars view engine support and create index.hbs template; include robots.txt for SEO

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Render } from "@nestjs/common";
 
 import { AppService } from "./app.service";
 
@@ -7,6 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Render("index")
   getHello(): { message: string } {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import type { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 
 import { AppModule } from "./app.module";
 
 const logger = new Logger("Bootstrap");
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const port = 3000;
+
+  app.useStaticAssets(join(__dirname, "..", "public"));
+  app.setBaseViewsDir(join(__dirname, "..", "views"));
+  app.setViewEngine("hbs");
 
   await app.listen(port);
 

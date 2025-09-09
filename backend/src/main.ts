a535af8601cedd4ae302ad88b98b4850354d163b
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-09-09 19:11:52 -0300

    feat: integrate ConfigService to retrieve server configuration and log environment details

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,29 @@
 import { Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
+import type { EnvGlobalConfig } from "./configs/types";
 
 const logger = new Logger("Bootstrap");
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
+  const configService =
+    app.get<ConfigService<EnvGlobalConfig, true>>(ConfigService);
+
+  const { nodeEnv, port } =
+    configService.get<EnvGlobalConfig["server"]>("server");
+
   app.enableCors({
     origin: "*",
   });
 
-  const port = 3000;
-
   await app.listen(port);
 
   logger.log(`Application is running on: http://localhost:${port}`);
+  logger.log(`Environment: ${nodeEnv}`);
 }
 
 void bootstrap();

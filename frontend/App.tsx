2f30cdf5804e9757353fa2ae5e04a83f5bdef2a1
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-09-08 21:00:52 -0300

    feat: add server status check with socket.io in App component

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,13 +1,21 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3000");
 
 export default function App() {
+  const [serverIsAlive, setServerIsAlive] = useState(false);
+
+  socket.emit("check-alive", (data: { status: boolean }) => {
+    setServerIsAlive(data.status);
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Server is {serverIsAlive ? "alive" : "down"}</Text>
       <StatusBar style="auto" />
     </View>
   );

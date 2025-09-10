dc95230c91a78b5bd553a1ceee65be7fd7df9d77
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-09-09 22:49:46 -0300

    refactor: add type annotation for NestExpressApplication in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import type { NestExpressApplication } from "@nestjs/platform-express";
 
 import { AppModule } from "./app.module";
 import type { EnvGlobalConfig } from "./configs/types";
@@ -8,13 +9,17 @@ import type { EnvGlobalConfig } from "./configs/types";
 const logger = new Logger("Bootstrap");
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const configService =
     app.get<ConfigService<EnvGlobalConfig, true>>(ConfigService);
 
   const { nodeEnv, port } =
     configService.get<EnvGlobalConfig["server"]>("server");
+
+  if (nodeEnv === "production") {
+    app.set("trust proxy", true);
+  }
 
   app.enableCors({
     origin: "*",

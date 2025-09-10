ae12dfb7054f94231c17049c16bc3967c2089459
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-09-10 09:26:57 -0300

    feat: update GitHub OAuth redirect URIs for web and android platforms

--- frontend/src/actions/signInWithGitHub.ts
+++ frontend/src/actions/signInWithGitHub.ts
@@ -1,3 +1,4 @@
+import { Platform } from "react-native";
 import * as Linking from "expo-linking";
 import { v4 as uuidv4 } from "uuid";
 
@@ -6,7 +7,10 @@ import { githubPublic } from "../utils/env/github";
 const { githubOauthEndpoint, githubClientId, githubRedirectUri } = githubPublic;
 
 export default async function signInWithGitHub() {
-  const randomState = uuidv4();
+  const randomState =
+    Platform.OS === "web"
+      ? uuidv4()
+      : Math.random().toString(36).substring(2, 15);
 
   const authUrl = `${githubOauthEndpoint}?client_id=${githubClientId}&redirect_uri=${githubRedirectUri}&state=${randomState}`;
 
--- frontend/src/utils/env/github.ts
+++ frontend/src/utils/env/github.ts
@@ -1,11 +1,18 @@
+import { Platform } from "react-native";
+
 const githubOauthEndpoint: string | undefined =
   process.env.EXPO_PUBLIC_GITHUB_OAUTH_ENDPOINT;
 
 const githubClientId: string | undefined =
   process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID;
 
-const githubRedirectUri: string | undefined =
-  process.env.EXPO_PUBLIC_GITHUB_REDIRECT_URI;
+let githubRedirectUri: string | undefined;
+
+if (Platform.OS === "web") {
+  githubRedirectUri = process.env.EXPO_PUBLIC_GITHUB_REDIRECT_WEB_URI;
+} else if (Platform.OS === "android") {
+  githubRedirectUri = process.env.EXPO_PUBLIC_GITHUB_REDIRECT_ANDROID_URI;
+}
 
 if (!githubOauthEndpoint || !githubClientId || !githubRedirectUri) {
   throw new Error("GitHub OAuth environment variables are not set properly.");

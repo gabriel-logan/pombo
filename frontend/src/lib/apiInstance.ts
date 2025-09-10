e8a021a68e3eaac208334132ea1f761fe253ef54
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-09-10 01:31:58 -0300

    refactor: integrate useAuthStore for token management in apiInstance and socketInstance

--- frontend/src/lib/apiInstance.ts
+++ frontend/src/lib/apiInstance.ts
@@ -1,9 +1,20 @@
 import axios from "axios";
 
+import { useAuthStore } from "../stores/authStore";
 import { apiPublic } from "../utils/env/api";
 
 const apiInstance = axios.create({
   baseURL: apiPublic.serverApiBaseUrl,
+});
+
+apiInstance.interceptors.request.use((config) => {
+  const token = useAuthStore.token;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default apiInstance;
--- frontend/src/lib/socketInstance.ts
+++ frontend/src/lib/socketInstance.ts
@@ -1,7 +1,12 @@
 import { io } from "socket.io-client";
 
+import { useAuthStore } from "../stores/authStore";
 import { apiPublic } from "../utils/env/api";
 
-const socket = io(apiPublic.serverApiBaseUrl);
+const socket = io(apiPublic.serverApiBaseUrl, {
+  auth: {
+    token: useAuthStore.token,
+  },
+});
 
 export default socket;
